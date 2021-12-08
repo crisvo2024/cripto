1d966399f1d0e1c2cf51f85d0d3503501564d53f
Cristian Vargas <crvargaso@unal.edu.co>
2021-12-07 19:33:30 -0500

    AES

--- AES.py
+++ AES.py
@@ -9,13 +9,20 @@ def main():
     image = open(name, "rb")
     image_bytes = image.read()
     image.close()
-    key = bytearray("qwertyuiopasdfghjklnzxcvbnmqwert", "ascii")
+    security = int(input("enter the level of security (1.128/2.192/3.256): "))
+    if security == 1:
+        key = bytearray("qwertyuiopasdfgh", "ascii")
+    elif security == 2:
+        key = bytearray("qwertyuiopasdfghjklnzxcv", "ascii")
+    else:
+        key = bytearray("qwertyuiopasdfghjklnzxcvbnmqwert", "ascii")
     encoder = pyaes.AESModeOfOperationCTR(key)
-    des_encoded = encoder.encrypt(image_bytes)
-    encoded = base64.b64encode(des_encoded)
+    aes_encoded = encoder.encrypt(image_bytes)
+    encoded = base64.b64encode(aes_encoded)
     print(encoded)
     decoded64 = base64.b64decode(encoded)
-    decoded = encoder.decrypt(decoded64)
+    decoder = pyaes.AESModeOfOperationCTR(key)
+    decoded = decoder.decrypt(decoded64)
     new_name = "new_image."+name.split(".")[-1]
     new_image = open(new_name, "wb")
     new_image.write(decoded)
